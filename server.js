948514d417bebf4ced218978951b5f2955bb298b
Yash Jain <yjain@cainc.com>
2019-03-26 12:39:12 +0530

    change in server

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+    app.use(express.static('dist'));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         console.log(path);
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
     })
 }
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
